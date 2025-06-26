97ab55e8e9106ba14535bae8490b9fac36ab72c8
up-jacky <149249055+up-jacky@users.noreply.github.com>
2025-06-26 10:20:46 +0800

    feat: add character limits to inputs and separate form into different components / assets

---
+++ src/lib/svelte/Label.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let { required = false, forLabel, children } = $props()
+</script>
+
+{#if required}
+<label for="{forLabel}">{@render children()}<span class="text-red-500"> *</span></label>
+{:else}
+<label for="{forLabel}">{@render children()}</label>
+{/if}
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -5,40 +5,29 @@
     import { smoothScrollTo } from '$lib/functions/function';
     import { goto } from '$app/navigation';
 	import { ROUTE } from '../routes';
+	import Form from '$lib/svelte/Form.svelte';
+	import Heading from '$lib/svelte/Heading.svelte';
 
-    let isRegistering = false;
+    let isSigningUp = false;
     let leaving = false;
+    let formType = "login";
 
-    let showPassword: boolean = false;
-    let showconfirmpassword: boolean = false;
-    let password: string = '';
-    let confirmpassword : string = '';
-
-	$: if (password === '' && showPassword) {
-		setTimeout(() => {
-			showPassword = false;
-		}, 300);
-	}
-
-    $: if (confirmpassword === '' && showconfirmpassword) {
-		setTimeout(() => {
-			showconfirmpassword = false;
-		}, 300);
-	}
-
-	function toggle_mode() {
+	function toggleMode() {
         leaving = true;
 		setTimeout(() => {
-			isRegistering = !isRegistering;
+			isSigningUp = !isSigningUp;
             leaving = false;
-		}, 250); 
-	}
+		}, 250);
+        if(formType == 'login') formType = 'signup'
+        else formType = 'login'
+        console.log('From page', formType)
+	}   
 
     export let data: { id: string | null }
 
     // Mano - mano pa
     let dataString: string = '';
-    function submit_form(e: Event) {
+    function submitForm(e: Event) {
         e?.preventDefault();
         if(data && data.id) {
             goto(`${ROUTE.MODULES}-${data.id}`)
@@ -57,8 +46,8 @@
 </svelte:head>
 
 <div class="relative flex w-full h-screen justify-center items-center bg-cover bg-center bg-no-repeat" style="background-image: url({'/NISMEDfrontpage.jpg'})">
-    <div class="absolute inset-0 z-0 bg-black/50">g</div>
-	<div class="z-15 w-[400px] bg-white rounded-2xl shadow-xl px-6 py-10 transition-all duration-500 {isRegistering ? 'h-[650px]' : 'h-[520px]'} overflow-hidden">
+    <div class="absolute inset-0 z-0 bg-black/50"></div>
+	<div class="custom-morph z-15 w-[400px] {isSigningUp? 'h-[620px]': 'h-[450px]'} bg-white rounded-2xl shadow-xl px-8 py-8 transition-all ease-in-out duration-500 overflow-hidden">
 
 		<!-- Logo -->
 		<div class="flex justify-center gap-2 mb-6">
@@ -68,77 +57,22 @@
 
 		<!-- Title -->
         <div class="h-[40px] mb-5 w-full flex">
-            <h1 class="font-semibold text-3xl {leaving ? "morph-leave" : "morph-enter"}">{isRegistering ? 'Register' : 'Login'}</h1>
-        </div>
-
-		<!-- Form -->
-		<form class="flex flex-col gap-3" on:submit={submit_form}>
-            <div class="flex flex-col gap-1 {isRegistering ? "morph-enter" : "absolute opacity-0"}">
-                <label for="email">Email<span class="text-red-500 "> *</span></label>
-                <input type="text" id="email" name="email" class="w-full py-1 px-3 border-2 border-[#C5C5C5] rounded-lg shadow-[0_4px_2px_rgba(0,0,0,0.2)]" />
-            </div>
-
-			<div class="flex flex-col gap-1">
-				<label for="username">Username<span class="text-red-500"> *</span></label>
-                <input type="text" id='username' name='username' class="w-full py-1 px-3 border-2 border-[#C5C5C5] rounded-lg shadow-[0_4px_2px_rgba(0,0,0,0.2)]">
-			</div>
-
-			<div class="flex flex-col gap-1 relative">
-				<label for="password">Password<span class="text-red-500"> *</span></label>
-				<input bind:value={password} type={showPassword ? 'text' : 'password'} id="password" name="password"
-					class="w-full py-1 px-3 pr-10 border-2 border-[#C5C5C5] rounded-lg shadow-[0_4px_2px_rgba(0,0,0,0.2)]" />
-
-				<button type="button"
-					class={`absolute right-3 bottom-2 text-sm text-[#C5C5C5] transition duration-300 ${password ? 'opacity-100' : 'opacity-0'}`}
-					on:click={() => showPassword = !showPassword}>
-					{#if showPassword}
-						<FontAwesomeIcon icon={faEyeSlash} />
-					{:else}
-						<FontAwesomeIcon icon={faEye} />
-					{/if}
-				</button>
-			</div>
-
-            <div class="flex flex-col gap-1 {isRegistering ? "relative morph-enter" : "absolute opacity-0"}">
-                <label for="confirmpassword">Confirm Password<span class="text-red-500"> *</span></label>
-                <input bind:value={confirmpassword} type={showconfirmpassword ? 'text' : 'password'}
-                    id="confirmpassword" name="confirmpassword"
-                    class="w-full py-1 px-3 pr-10 border-2 border-[#C5C5C5] rounded-lg shadow-[0_4px_2px_rgba(0,0,0,0.2)]" />
-                    
-
-                <button type="button"
-                    class={`absolute right-3 bottom-2 text-sm text-[#C5C5C5] transition duration-300 ${confirmpassword ? 'opacity-100' : 'opacity-0'}`}
-                    on:click={() => showconfirmpassword = !showconfirmpassword}>
-                    {#if showconfirmpassword}
-                        <FontAwesomeIcon icon={faEyeSlash} />
-                    {:else}
-                        <FontAwesomeIcon icon={faEye} />
-                    {/if}
-                </button>
-            </div>
-
-			<!-- Submit -->
-			<button type="submit"
-				class="w-full bg-[#185A37] text-white rounded-xl p-2 mt-4 transition duration-300 ease-in-out hover:bg-[#146e47] hover:scale-[1.01]">
-				{isRegistering ? 'Register' : 'Log In'}
-			</button>
-
-			<!-- <button type="submit"
-				class="w-full bg-[#185A37] text-white rounded-xl p-2 mt-4 hover:bg-[#146e47] group overflow-hidden h-[40px]">
-                <span class="flex flex-col transition-transform duration-300 group-hover:-translate-y-[65px] gap-10">
-                    <span class="block">{isRegistering ? 'Register' : 'Log In'}</span>
-                    <span class="block">{isRegistering ? 'Log In' : 'Register'}</span>
+            <Heading style="form">
+                <span class={leaving ? "morph-leave" : "morph-enter"}>
+                    {isSigningUp ? 'Signup' : 'Login'}
                 </span>
-			</button> -->
+            </Heading>
+        </div>
+        
+        <Form bind:type="{formType}" onSubmit={submitForm}></Form>
 
-            <!-- Switch Forms -->
-            <div class="text-center text-sm text-black p-1 {leaving ? "morph-leave" : "morph-enter"}">
-                {isRegistering ? 'Already have an account?' : 'Don\'t have an account yet?'}
-                <button type="button" class="underline-container"
-                on:click={toggle_mode}
-                >{isRegistering ? 'Login' : 'Signup'}</button>
-            </div>
-		</form>
+        <!-- Switch Forms -->
+        <div class="text-center text-sm text-black p-1 {leaving ? "morph-leave" : "morph-enter"}">
+            {isSigningUp ? 'Already have an account?' : 'Don\'t have an account yet?'}
+            <button type="button" class="underline-container"
+            on:click={toggleMode}
+            >{isSigningUp ? 'Login' : 'Signup'}</button>
+        </div>
 	</div>
 </div>
 
@@ -163,10 +97,14 @@
     }
 
     .morph-enter {
-        animation: morphIn 500ms ease forwards;
+        animation: morphIn 300ms ease forwards;
     }
 
     .morph-leave {
-        animation: morphOut 500ms ease forwards;
+        animation: morphOut 300ms ease forwards;
+    }
+
+    .custom-morph {
+        animation: morphIn 600ms ease-in forwards;
     }
 </style>
