e5cd3a49cc11b1ac9f579fdb4a75451486fcb906
up-jacky <149249055+up-jacky@users.noreply.github.com>
2025-06-30 15:27:25 +0800

    fix: remove extra Tab in else statement

--- src/routes/program-[value]/+page.svelte
+++ src/routes/program-[value]/+page.svelte
@@ -190,87 +190,86 @@
 {/if}
 
 {#if !data.selectedItem}
-	<div class="flex flex-col w-full min-h-[calc(100dvh-120px)] mt-[120px] justify-center items-center">
-		<div class="flex flex-col w-[65%] flex-grow gap-10 py-20">
-			<div class="flex w-full h-[5%] justify-center items-center">
-				<h1 class="text-4xl font-semibold">All Training Modules</h1>
-				<div class="flex-grow"></div>
+<div class="flex flex-col w-full min-h-[calc(100dvh-120px)] mt-[120px] justify-center items-center">
+	<div class="flex flex-col w-[65%] flex-grow gap-10 py-20">
+		<div class="flex w-full h-[5%] justify-center items-center">
+			<h1 class="text-4xl font-semibold">All Training Modules</h1>
+			<div class="flex-grow"></div>
 
-				<div class="flex h-[30px] justify-center items-center border-b border-black">
-					<input 
-					type="text" 
-					placeholder="Search" 
-					class="w-[23ch] p-0 bg-transparent text-sm text-black placeholder-[rgb(0,0,0,0.5)] border-none focus:ring-0"
-					>
-					<button type = "button" class = "flex justify-center items-center w-10 h-10 p-1"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-				</div>
+			<div class="flex h-[30px] justify-center items-center border-b border-black">
+				<input 
+				type="text" 
+				placeholder="Search" 
+				class="w-[23ch] p-0 bg-transparent text-sm text-black placeholder-[rgb(0,0,0,0.5)] border-none focus:ring-0"
+				>
+				<button type = "button" class = "flex justify-center items-center w-10 h-10 p-1"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
 			</div>
-			<div class="flex w-full h-[90%]">
-				<!-- Populate dynamically once we have data storage -->
-				<div id="griditems" class="grid grid-cols-1 grid-flow-row w-full h-full gap-7">
-					{#each data.program.items as item}
-						<div class="scroll-card opacity-0 flex w-full rounded-3xl gap-2 border-2 border-gray-100">
-							<div
-								class="flex h-full w-[45%] border-r border-gray-200 rounded-l-3xl rounded-r-none"
-								style={`background: ${item.imageUrl ? `url('${item.imageUrl}') center/cover no-repeat` : 'var(--font-green)'}`}
-							></div>
-							<div class="flex h-full flex-grow p-2 pr-5">
-								<div class="flex flex-col w-full h-full rounded-3xl gap-2">
-									<h1 class="text-lg font-semibold">{item.title}</h1>
-									<div class="flex flex-col w-full h-full gap-1">
-										<div class="flex gap-2 items-center">
-											<p class = "font-light text-sm">Module Completeness</p>
-										</div>
-										<div class="flex gap-2 items-center">
-											<FontAwesomeIcon icon = {faChartSimple} class = "text-[var(--font-green)]"/>
-											<p class="font-light text-sm">{ 'leader' in item ? item.leader : 'Project Leader' }</p>
-										</div>
-										<div class="flex gap-1 items-center">
-											<FontAwesomeIcon icon = {faGraduationCap} class = "text-[var(--font-green)]"/>
-											<p class="font-light text-sm">{item.numParticipants}</p>
-										</div>
-										<div class="flex gap-2 items-center">
-											<FontAwesomeIcon icon = {faFile} class = "text-[var(--font-green)]"/>
-											<p class="font-light text-sm">
-												{training.find(t => t.id === item.trainingId)?.type ?? 'Unknown'}
-											</p>
-										</div>
-										<div class="flex gap-2 items-center">
-											<FontAwesomeIcon icon = {faClock} class = "text-[var(--font-green)]"/>
-											<p class="font-light text-sm">{handleDate(item.date)}</p>
-										</div>
+		</div>
+		<div class="flex w-full h-[90%]">
+			<!-- Populate dynamically once we have data storage -->
+			<div id="griditems" class="grid grid-cols-1 grid-flow-row w-full h-full gap-7">
+				{#each data.program.items as item}
+					<div class="scroll-card opacity-0 flex w-full rounded-3xl gap-2 border-2 border-gray-100">
+						<div
+							class="flex h-full w-[45%] border-r border-gray-200 rounded-l-3xl rounded-r-none"
+							style={`background: ${item.imageUrl ? `url('${item.imageUrl}') center/cover no-repeat` : 'var(--font-green)'}`}
+						></div>
+						<div class="flex h-full flex-grow p-2 pr-5">
+							<div class="flex flex-col w-full h-full rounded-3xl gap-2">
+								<h1 class="text-lg font-semibold">{item.title}</h1>
+								<div class="flex flex-col w-full h-full gap-1">
+									<div class="flex gap-2 items-center">
+										<p class = "font-light text-sm">Module Completeness</p>
 									</div>
-									<div class="relative flex w-full h-full border-t border-gray-200 justify-center items-center">
-										<button class="flex h-full p-1 font-medium hover:text-[var(--font-green)] hover:tracking-wide transition-all duration-300 ease-in-out cursor-pointer"
-										onclick={() => open_item(item.id.toString())}
-										>View More</button>
+									<div class="flex gap-2 items-center">
+										<FontAwesomeIcon icon = {faChartSimple} class = "text-[var(--font-green)]"/>
+										<p class="font-light text-sm">{ 'leader' in item ? item.leader : 'Project Leader' }</p>
+									</div>
+									<div class="flex gap-1 items-center">
+										<FontAwesomeIcon icon = {faGraduationCap} class = "text-[var(--font-green)]"/>
+										<p class="font-light text-sm">{item.numParticipants}</p>
+									</div>
+									<div class="flex gap-2 items-center">
+										<FontAwesomeIcon icon = {faFile} class = "text-[var(--font-green)]"/>
+										<p class="font-light text-sm">
+											{training.find(t => t.id === item.trainingId)?.type ?? 'Unknown'}
+										</p>
+									</div>
+									<div class="flex gap-2 items-center">
+										<FontAwesomeIcon icon = {faClock} class = "text-[var(--font-green)]"/>
+										<p class="font-light text-sm">{handleDate(item.date)}</p>
 									</div>
 								</div>
-							</div>		
-						</div>
-					{/each}
-				</div>
+								<div class="relative flex w-full h-full border-t border-gray-200 justify-center items-center">
+									<button class="flex h-full p-1 font-medium hover:text-[var(--font-green)] hover:tracking-wide transition-all duration-300 ease-in-out cursor-pointer"
+									onclick={() => open_item(item.id.toString())}
+									>View More</button>
+								</div>
+							</div>
+						</div>		
+					</div>
+				{/each}
 			</div>
-			<!-- Fix pagination once data storage is available -->
-			<div class="flex w-full flex-grow justify-center items-center">
-				<div class="flex-grow"></div>
-				<div class="grid grid-rows-1 grid-flow-col h-full gap-9">
-					<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black "><FontAwesomeIcon icon={faAngleLeft} /></button>
-					<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">1</button>
-					<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">2</button>
-					<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">3</button>
-					<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">4</button>
-					<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black "><FontAwesomeIcon icon={faAngleRight} /></button>
-				</div>
-				<div class="flex-grow"></div>
-			</div>
-			<button onclick={() => showModal = true} class="w-full bg-[#185A37] text-white rounded-xl p-2 mt-4 transition duration-300 ease-in-out hover:bg-[#146e47] hover:scale-[1.01]">
-				Add Training Module
-			</button>
-
 		</div>
-	</div>
+		<!-- Fix pagination once data storage is available -->
+		<div class="flex w-full flex-grow justify-center items-center">
+			<div class="flex-grow"></div>
+			<div class="grid grid-rows-1 grid-flow-col h-full gap-9">
+				<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black "><FontAwesomeIcon icon={faAngleLeft} /></button>
+				<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">1</button>
+				<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">2</button>
+				<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">3</button>
+				<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black">4</button>
+				<button class="flex w-10 h-10 justify-center items-center p-1 rounded-full border border-black "><FontAwesomeIcon icon={faAngleRight} /></button>
+			</div>
+			<div class="flex-grow"></div>
+		</div>
+		<button onclick={() => showModal = true} class="w-full bg-[#185A37] text-white rounded-xl p-2 mt-4 transition duration-300 ease-in-out hover:bg-[#146e47] hover:scale-[1.01]">
+			Add Training Module
+		</button>
 
+	</div>
+</div>
 {:else}
 <!-- Top Bar -->
 <div class="flex flex-col w-full min-h-[calc(100dvh-120px)] mt-[120px] items-center">
@@ -317,11 +316,6 @@
 
 	<!-- Content Area -->
 	<div class="flex w-full justify-center px-4 flex-col items-center gap-10">
-		<div class="flex flex-col w-full max-w-[900px]">
-			<!-- Tabs + Content Box -->
-			<div class=" bg-white border border-black/10 rounded-xl shadow-sm overflow-hidden">
-				<!-- Tabs -->
-				<Tab tabs={tabs} bind:activeTab/>
 		<div class="flex flex-col w-full max-w-[900px]">
 			<!-- Tabs + Content Box -->
 			<div class=" bg-white border border-black/10 rounded-xl shadow-sm overflow-hidden">
