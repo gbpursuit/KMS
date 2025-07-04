4ffe6246a4acc3f484dae1818c8e7243307869f8
gbpursuit <glcoronel@up.edu.ph>
2025-07-04 13:04:21 +0800

    fix: bug sessionExpire (not checked 100%)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,12 +17,12 @@
 	<Header currentPath={currentPath} data = {data}/>
 {/if}
 {@render children()}
-{#if sessionExpired() && currentPath !== ROUTE.AUTH}
+{#if sessionExpired() && currentPath !== ROUTE.ROOT && currentPath !== ROUTE.AUTH}
 	<div class="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
 		<div class="bg-white p-6 rounded-lg shadow-lg text-center max-w-sm w-full">
-			<h2 class="text-xl font-bold text-red-600">Session Expired</h2>
+			<h2 class="text-xl font-bold text-[var(--font-green)]">Session Expired</h2>
 			<p class="mt-2 text-gray-700">Please log in again to continue.</p>
-			<a href="/auth" class="mt-4 inline-block bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition">
+			<a href="/auth" class="mt-4 inline-block bg-[var(--font-green)] text-white px-4 py-2 rounded hover:bg-[var(--hover-green)] transition">
 				Go to Login
 			</a>
 		</div>
